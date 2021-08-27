4a55c4611e4a99e6ee8d27c9f356f55f870615c5
AaryanKh <66236478+AaryanKhClasses@users.noreply.github.com>
2021-08-27 11:14:59 +0530

    fixed antiraid for new trigger-ers lol

--- modules/antiraid.js
+++ modules/antiraid.js
@@ -21,41 +21,42 @@ module.exports = (client) => {
         if(member.roles.cache.find(r => r.name.includes('Trusted'))) return
 
         const data = await antiraid.findOne({ guildID: role.guild.id, userID: member.id })
-        let ROLE_CREATE_NUM = data.ROLE_CREATE
-        if(!ROLE_CREATE_NUM) ROLE_CREATE_NUM = 0
         if(data) {
-            await antiraid.findOneAndUpdate({ guildID: role.guild.id, userID: member.id }, { guildID: role.guild.id, userID: member.id, ROLE_CREATE: ROLE_CREATE_NUM + 1 })
+            let ROLE_CREATE_NUM = data.ROLE_CREATE
+            if(!ROLE_CREATE_NUM) ROLE_CREATE_NUM = 0
+                await antiraid.findOneAndUpdate({ guildID: role.guild.id, userID: member.id }, { guildID: role.guild.id, userID: member.id, ROLE_CREATE: ROLE_CREATE_NUM + 1 })
             console.log(ROLE_CREATE_NUM)
             setTimeout(async () => {
                 while(ROLE_CREATE_NUM > 0) {
                     await antiraid.findOneAndUpdate({ guildID: role.guild.id, userID: member.id }, { guildID: role.guild.id, userID: member.id, ROLE_CREATE: ROLE_CREATE_NUM - 1 })
                 }
             }, 1800000)
-        } else {
+
+            let limit = 5
+            if(ROLE_CREATE_NUM > limit) {
+                let quarantinedRole
+                if(role.guild.roles.cache.find(r => r.name.includes('Quarantined'))) quarantinedRole = role.guild.roles.cache.find(r => r.name.includes('Quarantined'))
+                else quarantinedRole = await role.guild.roles.create({ name: 'Quarantined', reason: 'Quarantined role required by Wend\'s Anti Raid Feature.' })
+
+                const allChannels = role.guild.channels.cache.filter(ch => ch.type !== 'GUILD_CATEGORY')
+                allChannels.forEach(ch => ch.permissionOverwrites.create(quarantinedRole, { SEND_MESSAGES: false, ADD_REACTIONS: false, VIEW_CHANNEL: false }))
+
+                member.roles.set([quarantinedRole])
+                const embed = new MessageEmbed()
+                .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL({ dynamic: true }))
+                .setColor('GREEN')
+                .setTitle('Member Quarantined.')
+                .setDescription(`${emojis.user} **Member:** ${member.user.tag}\n${emojis.id} **Member ID:** ${member.id}\n${emojis.role} **All Roles Cleansed:** ${emojis.success}\n **Member Quarantined:** ${emojis.success}`)
+                .addField(`${emojis.description} Reason`, `Member tried to raid the server by breaking the role creation limit.`)
+                .setTimestamp()
+                .setFooter(botname)
+                logsChannel.send({ embeds: [ embed ] })
+            }
+            } else {
             const newData = new antiraid({ guildID: role.guild.id, userID: member.id })
             newData.save()
             await antiraid.findOneAndUpdate({ guildID: role.guild.id, userID: member.id }, { guildID: role.guild.id, userID: member.id, ROLE_CREATE: 1 })
         }
 
-        let limit = 5
-        if(ROLE_CREATE_NUM > limit) {
-            let quarantinedRole
-            if(role.guild.roles.cache.find(r => r.name.includes('Quarantined'))) quarantinedRole = role.guild.roles.cache.find(r => r.name.includes('Quarantined'))
-            else quarantinedRole = await role.guild.roles.create({ name: 'Quarantined', reason: 'Quarantined role required by Wend\'s Anti Raid Feature.' })
-
-            const allChannels = role.guild.channels.cache.filter(ch => ch.type !== 'GUILD_CATEGORY')
-            allChannels.forEach(ch => ch.permissionOverwrites.create(quarantinedRole, { SEND_MESSAGES: false, ADD_REACTIONS: false, VIEW_CHANNEL: false }))
-
-            member.roles.set([quarantinedRole])
-            const embed = new MessageEmbed()
-            .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL({ dynamic: true }))
-            .setColor('GREEN')
-            .setTitle('Member Quarantined.')
-            .setDescription(`${emojis.user} **Member:** ${member.user.tag}\n${emojis.id} **Member ID:** ${member.id}\n${emojis.role} **All Roles Cleansed:** ${emojis.success}\n **Member Quarantined:** ${emojis.success}`)
-            .addField(`${emojis.description} Reason`, `Member tried to raid the server by breaking the role creatiom limit.`)
-            .setTimestamp()
-            .setFooter(botname)
-            logsChannel.send({ embeds: [ embed ] })
-        }
     })
 }
